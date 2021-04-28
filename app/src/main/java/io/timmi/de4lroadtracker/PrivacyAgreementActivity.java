c6dae254ab9237866461ca84a7da2f912aab3e56
winzlieb <winzlieb@c3d2.de>
2021-04-28 12:03:33 +0200

    privacy agreement activity

--- app/src/main/java/io/timmi/de4lroadtracker/PrivacyAgreementActivity.java
+++ app/src/main/java/io/timmi/de4lroadtracker/PrivacyAgreementActivity.java
@@ -11,20 +11,35 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.CheckBox;
 
+import io.timmi.de4lroadtracker.helper.Md5Builder;
 import io.timmi.de4lroadtracker.helper.RawResourceLoader;
 import us.feras.mdv.MarkdownView;
 
 public class PrivacyAgreementActivity extends AppCompatActivity {
 
+    private String agreementText = null;
+
+    private boolean didAgree() {
+
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        if(sharedPreferences.contains("agreedPrivacyMD5") && agreementText != null) {
+            try {
+                return Md5Builder.md5(agreementText).equals(sharedPreferences.getString("agreedPrivacyMD5", ""));
+            } catch (Exception e) { }
+        }
+        return false;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        agreementText = RawResourceLoader.loadText(R.raw.privacy, this);
         setContentView(R.layout.activity_privacy_agreement);
         MarkdownView markdownView = (MarkdownView) findViewById(R.id.markdownView);
-        markdownView.loadMarkdown(RawResourceLoader.loadText(R.raw.privacy, this));
+        markdownView.loadMarkdown(agreementText);
         CheckBox agreePrivacyCheckbox = (CheckBox) findViewById(R.id.agreeCheckbox);
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-        if(sharedPreferences.getBoolean("hasPrivacyAgreement", false)) {
+        if(didAgree()) {
            agreePrivacyCheckbox.setChecked(true);
         }
     }
@@ -40,7 +55,9 @@ public class PrivacyAgreementActivity extends AppCompatActivity {
     public void handleClickAgreeCheckbox(View checkBox) {
         //Log.d("Privacy", String.valueOf(((CheckBox)checkBox).isChecked()));
         SharedPreferences.Editor sharedPreferencesEditor = PreferenceManager.getDefaultSharedPreferences(this).edit();
+        String md5 = Md5Builder.md5(agreementText);
         sharedPreferencesEditor.putBoolean("hasPrivacyAgreement", ((CheckBox)checkBox).isChecked());
+        sharedPreferencesEditor.putString("agreedPrivacyMD5", md5);
         sharedPreferencesEditor.apply();
 
     }
